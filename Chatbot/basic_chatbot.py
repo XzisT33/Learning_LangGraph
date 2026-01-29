d1ea7600ac1eb27683c280d323f4d39cd09f3384
XzcutE33 <aryanp12042002@gmail.com>
2026-01-29 23:28:16 +0530

    Added a basic chatbot implementation.

---
+++ Chatbot/basic_chatbot.py
@@ -0,0 +1,56 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+#Define the state
+class ChatbotState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+#Define the nodes
+def llm_convo(state: ChatbotState):
+    messages = state["messages"]
+
+    response = model.invoke(messages)
+
+    return {'messages': [response]}
+
+
+#Define the graph
+graph = StateGraph(ChatbotState)
+checkpointer = MemorySaver()
+
+graph.add_node('llm_chat', llm_convo)
+
+graph.add_edge(START, 'llm_chat')
+graph.add_edge('llm_chat', END)
+
+chatbot = graph.compile(checkpointer= checkpointer)
+
+
+
+thread_id = 1
+
+while True:
+    user_input = input("User: ")
+    if user_input.strip().lower() in ['exit', 'bye']:
+        break
+
+    config = {'configurable': {'thread_id': thread_id}}
+    response = chatbot.invoke({'messages': [HumanMessage(content= user_input)]}, 
+                              config= config)
+    print("AI: ", response['messages'][-1].content)
