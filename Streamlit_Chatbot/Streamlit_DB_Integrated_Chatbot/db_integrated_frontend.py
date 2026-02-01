9e17be4bc7c300b842451d14538ab39e0d711ac3
XzcutE33 <aryanp12042002@gmail.com>
2026-02-01 17:18:18 +0530

    Integrated Langsmith

--- Streamlit_Chatbot/Streamlit_DB_Integrated_Chatbot/db_integrated_frontend.py
+++ Streamlit_Chatbot/Streamlit_DB_Integrated_Chatbot/db_integrated_frontend.py
@@ -2,6 +2,9 @@ import streamlit as st
 from db_integrated_backend import chatbot, retrieve_threads
 from langchain_core.messages import HumanMessage
 import uuid
+import os
+
+os.environ['LANGCHAIN_PROJECT'] = 'LangGraph_Chatbot'
 
 
 #Utility Functions
@@ -65,6 +68,10 @@ for message in st.session_state['chat_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']},
+          'metadata': {'thread_id': st.session_state['thread_id']},
+          'run_name': 'chat_convo'
+          }
 
 user_input = st.chat_input("Enter your text: ")
 if user_input:
@@ -76,7 +83,7 @@ if user_input:
     with st.chat_message('assistant'):
         AI_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in chatbot.stream({'messages' : [HumanMessage(content= user_input)]}, 
-                                    config = {'configurable': {'thread_id': st.session_state['thread_id']}},
+                                    config = CONFIG,
                                     stream_mode = 'messages')
         )
 
